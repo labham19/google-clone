da17997c5baa242337c5123cbfaf1a729d153562
labham19 <labham19@gmail.com>
2022-08-03 14:27:19 +0530

    Responsiveness added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,11 +15,11 @@ const Home = () =>{
   return (
     <div className='h-full w-full'>  
       <form onSubmit={searchNow}>
-      <div className='flex flex-col justify-center items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 space-y-6'>
+      <div className='w-4/5 md:w-auto flex flex-col justify-center items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 space-y-6'>
           
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png" className='w-2/5'/>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png" className='w-4/5 md:w-2/5'/>
           
-          <div className='flex justify-between items-center w-2/3 px-4 py-2 border-2 rounded-3xl hover:bg-slate-100'>
+          <div className='flex justify-between items-center w-full md:w-2/3 px-4 py-2 border-2 rounded-3xl hover:bg-slate-100'>
             <i className="fa fa-search cursor-pointer" aria-hidden="true"></i>
             <input value={searchedVal} onChange={(e)=>setSearchedVal(e.target.value)} className='px-3 w-full focus:outline-none bg-transparent' />
             <i className="fa fa-microphone cursor-pointer hover:text-blue-600" aria-hidden="true"></i>
